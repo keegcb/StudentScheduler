4fd774dea489a9eed98ac28433c600ff981499f0
keegcb <keegcb@gmail.com>
2022-08-14 23:17:55 -0400

    Project creation, first commit

---
+++ app/src/main/java/com/example/studentscheduler/UI/TermDetails.java
@@ -0,0 +1,16 @@
+package com.example.studentscheduler.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.studentscheduler.R;
+
+public class TermDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_term_details);
+    }
+}
